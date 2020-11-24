93c8594d801b116e1068aaa0cd26d50c2cc61ede
bhanudobriyal <bdobriy1@binghamton.edu>
2020-11-24 15:34:52 -0500

    connect to MongoDb , create sample users

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/myshop', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
 
 app.get('/api/products/:id', (req, res) => {
     const product = data.products.find((x) => x._id === req.params.id);
@@ -15,10 +25,16 @@ app.get('/api/products/:id', (req, res) => {
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
-
+app.use('/api/users', userRouter);
 app.get('/', (req, res) => {
     res.send('Server is ready');
 });
+
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message });
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
