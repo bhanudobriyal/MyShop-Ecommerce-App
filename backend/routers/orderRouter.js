8f5fe50839923c96fa292c7950a2dee8c0c514de
bhanudobriyal <bdobriy1@binghamton.edu>
2020-11-25 03:16:57 -0500

    Implement Order payment

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -29,6 +29,7 @@ orderRouter.post(
         }
     })
 );
+
 orderRouter.get(
     '/:id',
     isAuth,
@@ -36,6 +37,28 @@ orderRouter.get(
         const order = await Order.findById(req.params.id);
         if (order) {
             res.send(order);
+        } else {
+            res.status(404).send({ message: 'Order Not Found' });
+        }
+    })
+);
+
+orderRouter.put(
+    '/:id/pay',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+        const order = await Order.findById(req.params.id);
+        if (order) {
+            order.isPaid = true;
+            order.paidAt = Date.now();
+            order.paymentResult = {
+                id: req.body.id,
+                status: req.body.status,
+                update_time: req.body.update_time,
+                email_address: req.body.email_address,
+            };
+            const updatedOrder = await order.save();
+            res.send({ message: 'Order Paid', order: updatedOrder });
         } else {
             res.status(404).send({ message: 'Order Not Found' });
         }
