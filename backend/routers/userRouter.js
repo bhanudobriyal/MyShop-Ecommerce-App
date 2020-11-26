cddb9b217228c84dbf26e4461b0c55cd83eec867
bhanudobriyal <bdobriy1@binghamton.edu>
2020-11-25 20:56:10 -0500

    display user profile

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -35,6 +35,7 @@ userRouter.post(
         res.status(401).send({ message: 'Invalid email or password' });
     })
 );
+
 userRouter.post(
     '/register',
     expressAsyncHandler(async (req, res) => {
@@ -53,4 +54,17 @@ userRouter.post(
         });
     })
 );
+
+userRouter.get(
+    '/:id',
+    expressAsyncHandler(async (req, res) => {
+        const user = await User.findById(req.params.id);
+        if (user) {
+            res.send(user);
+        } else {
+            res.status(404).send({ message: 'User Not Found' });
+        }
+    })
+);
+
 export default userRouter;
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,7 @@
 import {
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
     USER_REGISTER_FAIL,
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
@@ -7,6 +10,7 @@ import {
     USER_SIGNIN_SUCCESS,
     USER_SIGNOUT,
 } from '../constants/userConstants';
+
 export const userRegisterReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_REGISTER_REQUEST:
@@ -19,6 +23,7 @@ export const userRegisterReducer = (state = {}, action) => {
             return state;
     }
 };
+
 export const userSigninReducer = (state = {}, action) => {
     switch (action.type) {
         case USER_SIGNIN_REQUEST:
@@ -29,6 +34,18 @@ export const userSigninReducer = (state = {}, action) => {
             return { loading: false, error: action.payload };
         case USER_SIGNOUT:
             return {};
+        default:
+            return state;
+    }
+};
+export const userDetailsReducer = (state = { loading: true }, action) => {
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return { loading: true };
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload };
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
         default:
             return state;
     }
